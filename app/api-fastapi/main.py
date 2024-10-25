5b79ae148d5e38ab60d9a6ec2a6217467b6d3c1c
FWasik <franekwasik2@gmail.com>
2024-10-25 16:36:36 +0200

    chore-app: rename to goals and add styles

--- app/api-fastapi/main.py
+++ app/api-fastapi/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost", "http://kubernetes.docker.internal"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -21,17 +21,24 @@ MONGO_DB_NAME = os.getenv("MONGO_INITDB_DATABASE")
 
 client = AsyncIOMotorClient(f"mongodb://{MONGO_DB_USERNAME}:{MONGO_DB_PASSWORD}@mongodb:27017/{MONGO_DB_NAME}?authSource=admin")
 db = client.goalapp
-collection = db.tasks
+collection = db.goals
 
-class Task(BaseModel):
+class Goal(BaseModel):
     title: str
 
-@app.post("/api/tasks")
-async def create_task(task: Task):
-    result = await collection.insert_one(task.dict())
-    return {"id": str(result.inserted_id), "title": task.title}
+@app.post("/api/goals")
+async def create_goal(goal: Goal):
+    result = await collection.insert_one(goal.dict())
+    return {"id": str(result.inserted_id), "title": goal.title}
 
-@app.get("/api/tasks")
-async def get_tasks():
-    tasks = await collection.find().to_list(1000)
-    return [{"id": str(task["_id"]), "title": task["title"]} for task in tasks]
+@app.get("/api/goals")
+async def get_goals():
+    goals = await collection.find().to_list(1000)
+    return [{"id": str(goal["_id"]), "title": goal["title"]} for goal in goals]
+
+@app.delete("/api/goals/{goal_id}")
+async def delete_goal(goal_id: str):
+    result = await collection.delete_one({"_id": ObjectId(goal_id)})
+    if result.deleted_count == 0:
+        raise HTTPException(status_code=404, detail="Goal not found")
+    return {"status": "Goal deleted successfully"}
