881ae95aed78923403f2369f24d1536b5f4f0072
FWasik <franekwasik2@gmail.com>
2024-10-29 14:56:28 +0100

    chore-app: allow origins for CORS

--- app/api-fastapi/main.py
+++ app/api-fastapi/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost", "http://kubernetes.docker.internal"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
